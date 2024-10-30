0b40f1d4c4136d64a58aa6815af4291d4bce73d6
DevGabriel0402 <gabriel.profissional01@gmail.com>
2024-10-30 14:59:07 -0300

    Primeiro commit do Codigo

---
+++ index.js
@@ -0,0 +1,37 @@
+
+
+setTimeout(() => {
+    document.getElementById(`card-contrate`).style.animation = `enterCard .5s forwards`
+}, 2000)
+
+//Caso o tamanho da tela forma maior que 400 ele coloque 3 slide na tela
+if (innerWidth >= 400) {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 3,
+        // spaceBetween: 10,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+    // Senao ele coloque somente 1 slide
+} else {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        // spaceBetween: 30,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+    document.getElementById(``)
+}
+
