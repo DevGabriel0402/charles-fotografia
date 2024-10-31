233f8f5b6746d4d9ef1b71472096c383b521092f
DevGabriel0402 <gabriel.profissional01@gmail.com>
2024-10-31 10:14:51 -0300

    Inserindo o formulario e seus comando

--- index.js
+++ index.js
@@ -32,13 +32,43 @@ if (innerWidth >= 400) {
             prevEl: ".swiper-button-prev",
         },
     });
-    
+
 }
 
-document.getElementById('submit').addEventListener('click', ()=>{
+document.getElementById(`btn-contato`).addEventListener(`click`, () => {
+    document.getElementById(`modal`).style.display = `flex`;
+    document.getElementById(`card-contrate`).style.display = `none`;
+    document.getElementById(`container`).style.filter = `blur(5px)`
+})
+
+document.getElementById(`btn-fechar`).addEventListener(`click`, () => {
+    document.getElementById(`modal`).style.display = `none`;
+    document.getElementById(`card-contrate`).style.display = `flex`;
+    document.getElementById(`container`).style.filter = `blur(0)`
+
+
+})
+
+document.getElementById('submit').addEventListener('click', () => {
     const nome = document.getElementById('inputName').value
     const email = document.getElementById('inputEmail').value
     const texto = document.getElementById('inputText').value
 
-    const mensagem = `Olá Charles tudo bem? Meu nome é ${nome} e estou entrando em contato para fazer um orçamento`
+    const mensagem = `Olá Charles tudo bem? Meu nome é ${nome} e estou entrando em contato para fazer um orçamento de ${texto}.
+    
+    caso precise enviar alguma coisa e não pode ser pelo whatsApp aqui está o meu e-mail:
+    
+    ${email}.
+    
+    Aguardando sua resposta.`
+
+    const uri = encodeURIComponent(mensagem)
+
+    const contato = `31985127512`
+
+    const url = `https://wa.me/55${contato}?text=${uri}`
+
+    window.open(url, `_blank`)
+
+
 })
